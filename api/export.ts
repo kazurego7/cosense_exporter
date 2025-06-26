5b44602237855126fce6b6c988a0a8871078a2de
namnam <kazurego7@gmail.com>
2025-06-26 21:18:25 +0900

    fix zip export

--- api/export.ts
+++ api/export.ts
@@ -1,5 +1,5 @@
 import { fetchPages } from "../cosense.ts";
-import { streamZip } from "../zip.ts";
+import { buildZip } from "../zip.ts";
 
 export const config = { runtime: "vercel-deno@3.1.1" };
 
@@ -9,8 +9,8 @@ export default async function handler(req: Request): Promise<Response> {
   }
   const { project, sid } = await req.json();
   const pages = await fetchPages(project, sid);
-  const stream = streamZip(pages);
-  return new Response(stream, {
+  const data = await buildZip(pages);
+  return new Response(data, {
     headers: {
       "Content-Type": "application/zip",
       "Content-Disposition": "attachment; filename=export.zip",
